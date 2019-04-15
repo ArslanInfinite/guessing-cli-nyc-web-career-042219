f6d0e162641e32b40f8521464ebcdb9af018ab2f
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 22:16:22 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -14,11 +14,9 @@ end
 def run_guessing_game
   res = ""
   while res != "exit"
-    start_game
-    computer_guess = random_num
-    #puts computer_guess
+    game_start
+    computer_guess = random_number
     res = gets.chomp
-    #puts res
     if computer_guess == res.to_i
       puts "You guessed the correct number!"
     else

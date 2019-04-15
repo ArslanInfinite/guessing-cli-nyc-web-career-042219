36625d66f3af7ee1633c7d4e4b02dde2f089ae5d
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 22:14:19 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,12 +1,10 @@
 # Code your solution here!
 def run_guessing_game
   puts "Guess a number between 1 and 6."
+  puts "Goodbye!"
 end
 
 def random_num
   rand(1..6)
 end
 
-def exit_game
-  puts "Goodbye!"
-end
